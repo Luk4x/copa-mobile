4fc4482828ccd1afc59ef5c79fd48006c8700198
Luk4x <luk4xm4ci3l@gmail.com>
2022-11-03 21:59:29 -0300

    a1

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import Loading from './src/components/Loading';
+import SignIn from './src/screens/SignIn';
+
+export default function App() {
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+            {fontsLoaded ? <SignIn /> : <Loading />}
+        </NativeBaseProvider>
+    );
+}
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from 'native-base';
+
+export default function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+            <Text color="white" fontSize={25}>
+                SignIn Akame-chaaaaaaaan!
+            </Text>
+        </Center>
+    );
+}
